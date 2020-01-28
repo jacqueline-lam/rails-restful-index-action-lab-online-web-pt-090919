e984be92e6c7aeb256dafa002187ba673fed5297
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2020-01-28 22:21:37 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  # tells Rails routing system that the route shoule bd passed through student_controller's '/students' action 
+  # tells Rails routing system that the route shoule bd passed through students_controller's '/students' action 
   # a rails route maps to a URL to a controller and Action
-  get 'hello_world', to: 'static#hello_world'
+  get 'hello_world', to: 'students#index'
 end
