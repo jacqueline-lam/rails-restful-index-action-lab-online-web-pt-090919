6d48c53204164313a031dec8f761c97347c66501
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2020-01-28 22:16:31 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  # tells Rails routing system that the route shoule bd passed through static controller's hello_world action 
+  # a rails route maps to a URL to a controller and Action
+  get 'hello_world', to: 'static#hello_world'
 end
